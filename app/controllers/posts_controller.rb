6fbfc3c68716658aa53645a1c47d626673660eec
Demian Gemperli <demian.gemperli@gmail.com>
2010-12-17 11:29:14 +0100

    improvements on caching

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,15 +6,19 @@ class PostsController < ApplicationController
     redirect_to board_path(@board)
   end
   def image
-    convert_png(params[:id]) unless Rails.cache.read("img-"+params[:id])
-
-    send_data Rails.cache.read("img-"+params[:id]), :type => :png
+    cache_key = "img-"+params[:id]
+    img_data = Rails.cache.read(cache_key)
+    unless img_data
+      img_data = convert_png(params[:id])
+      Rails.cache.write(cache_key, img_data)
+    end
+    send_data img_data,:type => :png,:disposition => 'inline'
   end
   private
     def convert_png(id)
       require 'RMagick'
       img = Magick::Image.from_blob(Post.find(id).svg)
       img[0].format = "png"
-      Rails.cache.write("img-"+id,img[0].to_blob)
+      return img[0].to_blob
     end
 end
